baeb3895bf78680b926ee031d7d543b03c2e6dab
Michael Hermes <mikehermes87@gmail.com>
2021-09-16 14:35:48 -0700

    Add SET NULL foreign key configuration to allow deleting categories.

--- models/index.js
+++ models/index.js
@@ -4,8 +4,14 @@ const Category = require("./Category");
 const Tag = require("./Tag");
 const ProductTag = require("./ProductTag");
 
-Product.belongsTo(Category, { foreignKey: "category_id" });
-Category.hasMany(Product, { foreignKey: "category_id" });
+Product.belongsTo(Category, {
+	foreignKey: "category_id",
+	onDelete: "SET NULL",
+});
+Category.hasMany(Product, {
+	foreignKey: "category_id",
+	onDelete: "SET NULL",
+});
 Product.belongsToMany(Tag, { through: ProductTag, foreignKey: "product_id" });
 Tag.belongsToMany(Product, { through: ProductTag, foreignKey: "tag_id" });
 
